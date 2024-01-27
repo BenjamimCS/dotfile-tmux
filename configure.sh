e967dfb304311549886d32c3957322e5604966d5
BenjamimCS <uj1kjfomy@mozmail.com>
2024-01-27 07:55:26 -0300

    Operation now cancelable; SIGINT it's now handleable so ^c looks nice

--- configure.sh
+++ configure.sh
@@ -6,6 +6,12 @@
 # i.g.: there's .tmux.conf and .tmux.conf.1,
 # .tmux.conf becomes .tmux.conf.2
 message='\e[32m=> Add \e[37;4m.tmux.conf\e[0m\e[32m file in \e[33m$HOME\e[30m'
+trap go_off INT
+
+function go_off {
+  echo -e "\e[35m\nOperation canceled. Going off Zzzz\e[0m"
+  exit 1
+}
 
 function no_override {
   if ! [ -e ~/.tmux.conf.old ]; then
@@ -37,7 +43,7 @@ fi
 if [ -e "${HOME}/.tmux.conf" ]; then
   echo -e  "Override \e[37;4m.tmux.conf\e[0m? "
   echo -e  "\e[32mTIP:\e[0m Case doesn't matter"
-  echo -en "\e[30m[ \e[33mY \e[30mor \e[33myes \e[30m | \e[33mN \e[30mor \e[33mNo \e[30m]\e[33m "
+  echo -en "\e[30m[ \e[33mY \e[30mor \e[33myes \e[30m | \e[33mN \e[30mor \e[33mNo \e[30m] | \e[35m[Cancel or ^c]\e[33m "
   read confirm
   confirm=`echo $confirm | tr [:upper:] [:lower:]`
 fi
@@ -49,6 +55,8 @@ case $confirm in
   n | no)
     echo -e ${message}
     no_override;;
+  cancel)
+    go_off;;
   *)
     cat tmux.conf > ~/.tmux.conf;;
 esac
